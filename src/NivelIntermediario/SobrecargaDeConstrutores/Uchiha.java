83a442ea68a1fc4b4c8877cbc06c6add14253447
eduardo.zaniboni <eduardo.zaniboni@el.com.br>
2025-03-29 11:12:38 -0300

    Fiasco [Curso: Batismo de Java] - Vídeo Aula - Sobrecarga de Construtores

---
+++ src/NivelIntermediario/SobrecargaDeConstrutores/Uchiha.java
@@ -0,0 +1,22 @@
+package NivelIntermediario.SobrecargaDeConstrutores;
+
+public class Uchiha extends Ninja {
+
+    public Uchiha() {
+        super();
+    }
+
+    public Uchiha(String nome, String aldeia, int idade) {
+        super(nome, aldeia, idade);
+    }
+
+    public Uchiha(String nome, String aldeia, int idade, int numeroDeMissoesConcluidas, NivelNinja rank) {
+        super(nome, aldeia, idade, numeroDeMissoesConcluidas, rank);
+    }
+
+    @Override
+    public void habilidadeEspecial() {
+        System.out.println("Meu nome é " + nome + ". Sou um Uchiha. Minha habilidade especial é de fogo. Eu já complete: " + numeroDeMissoesConcluidas);
+    }
+
+}
