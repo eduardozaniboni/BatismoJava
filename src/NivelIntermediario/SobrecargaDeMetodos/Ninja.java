a7c004340f88f499aec462e47d5e08742ba2fa4f
eduardo.zaniboni <eduardo.zaniboni@el.com.br>
2025-03-29 11:11:23 -0300

    Fiasco [Curso: Batismo de Java] - Vídeo Aula - Sobrecarga de Métodos

---
+++ src/NivelIntermediario/SobrecargaDeMetodos/Ninja.java
@@ -0,0 +1,59 @@
+package NivelIntermediario.SobrecargaDeMetodos;
+
+public abstract class Ninja implements EstrategiaDeBatalha {
+    // TODO: Incluir novos 2 atributos: numeroDeMissoesConcluidas, Rank
+    // TODO: Rank: GENIN, CHuUNIN, JOUNIN, HOKAGE
+
+    String nome;
+    String aldeia;
+    int idade;
+    int numeroDeMissoesConcluidas;
+    NivelNinja rank;
+
+    public Ninja() {
+    }
+
+    public Ninja(String nome, String aldeia, int idade) {
+        this.nome = nome;
+        this.aldeia = aldeia;
+        this.idade = idade;
+    }
+
+    // TODO: Sobrecarga do construtor chamando os novos atributos
+    // Sobrecarga de métodos, você não precisa redeclarar o construtor, somente os novos atributos
+    public Ninja(String nome, String aldeia, int idade, int numeroDeMissoesConcluidas, NivelNinja rank) {
+        this(nome, aldeia, idade);
+        this.numeroDeMissoesConcluidas = numeroDeMissoesConcluidas;
+        this.rank = rank;
+    }
+
+    /* Metodos geral! Todos os ninjas vao ter */
+    public void habilidadeEspecial() {
+        System.out.println("Meu nome é " + nome + ". Sou um Ninja. Minha habilidade especial é normal.");
+    }
+
+    // Sobreescrevendo o metodo da interface
+    @Override
+    public void estrategiaDeBatalhaNinja() {
+        System.out.println("Essa é minha ESTRATEGIA de batalha");
+    }
+
+    // Inteligencia de combate
+    @Override
+    public void inteligenciaDeCombate() {
+        System.out.println("Essa é minha INTELIGENCIA de combate");
+    }
+
+    // Sobrecarga de metodo - Inteligencia de combate
+    @Override
+    public void inteligenciaDeCombate(int qi) {
+        if (qi > 150) {
+            System.out.println("Seu QI é: " + qi + " e você é um gênio");
+        } else if (qi >= 130) {
+            System.out.println("Seu QI é: " + qi + " e você é um ninja promissor");
+        } else {
+            System.out.println("Seu QI é: " + qi + " e você precisa treinar mais...");
+
+        }
+    }
+}
